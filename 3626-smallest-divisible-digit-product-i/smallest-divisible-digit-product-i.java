f7c49e1552c843b7e319034dd43ff32ad21972c2
Prajakta Takbhate <160892301+prajaktatakbhate2@users.noreply.github.com>
2026-08-06 16:38:02 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (14.66%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while (true) {
+            int num = n, product = 1;
+
+            while (num > 0) {
+                product *= num % 10;
+                num /= 10;
+            }
+
+            if (product % t == 0)
+                return n;
+
+            n++;
+        }
+    }
+}
